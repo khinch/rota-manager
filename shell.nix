b5aabf387cf44e3c9e150e46496bd06e9c44e35c
Kieren Hinch <khinch-github@nylonmoon.com>
2026-02-22 15:27:34 +0000

    [refactor] Update shell.nix to be more idiomatic

--- shell.nix
+++ shell.nix
@@ -1,12 +1,43 @@
-with (import <nixpkgs> { config.allowUnfree = true; });
-mkShell{
-  buildInputs = [
-    pgadmin4-desktopmode
-    podman
-    podman-compose
-    postgresql
-    postman
-    protobuf
-    sqlx-cli
-  ];
-}
+# https://wiki.nixos.org/wiki/Rust
+ 
+{
+  pkgs ? import <nixpkgs> { config.allowUnfree = true; },
+}:
+let
+  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+in
+pkgs.callPackage (
+  {
+    stdenv,
+    mkShell,
+    rust-analyzer,
+    rustup,
+    rustPlatform,
+  }:
+  mkShell {
+    strictDeps = true;
+    # Build time tools (compilers, generators, hooks) available in the dev shell
+    nativeBuildInputs = [
+      rust-analyzer
+      rustup
+      rustPlatform.bindgenHook
+    ];
+    # Libraries and programs needed at runtime, available in the dev shell
+    buildInputs =
+      [
+        pkgs.bruno
+        pkgs.pgadmin4-desktopmode
+        pkgs.podman
+        pkgs.podman-compose
+        pkgs.postgresql
+        pkgs.postman
+      ];
+    RUSTC_VERSION = overrides.toolchain.channel;
+    RUSTUP_TOOLCHAIN = overrides.toolchain.channel;
+    # https://github.com/rust-lang/rust-bindgen#environment-variables
+    shellHook = ''
+      export PATH="''${CARGO_HOME:-~/.cargo}/bin":"$PATH"
+      export PATH="''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-${stdenv.hostPlatform.rust.rustcTarget}/bin":"$PATH"
+    '';
+  }
+) { } # Override specific arguments e.g. { rustup = myPinnedRustup; }
